f5e4ebd77b7ef49b1000c8dd65e79a51f7bf19ba
Roman Proskuryakov <humbug@deeptown.org>
2020-07-14 13:03:47 +0300

    Add several tests for priorities and regex operators

---
+++ sana/tests/negate.rs
@@ -0,0 +1,27 @@
+use sana::{Sana, Spanned};
+
+#[test]
+fn token_break() {
+    #[derive(Clone, Copy, Sana, PartialEq, Debug)]
+    enum Token {
+        #[regex(".+" & !".* .*")]
+        AnyNotSpace,
+        #[token(" ")]
+        Space,
+
+        #[error]
+        Error,
+    }
+
+    let input = "x y";
+    let mut lexer = Token::lexer(&input);
+
+    let tok = lexer.next().unwrap();
+    assert_eq!(tok, Spanned{ value: Token::AnyNotSpace, start: 0, end: 1 });
+
+    let tok = lexer.next().unwrap();
+    assert_eq!(tok, Spanned{ value: Token::Space, start: 1, end: 2 });
+
+    let tok = lexer.next().unwrap();
+    assert_eq!(tok, Spanned{ value: Token::AnyNotSpace, start: 2, end: 3 });
+}
---
+++ sana/tests/priority.rs
@@ -0,0 +1,45 @@
+use sana::{Sana, Spanned};
+
+#[test]
+fn priority_first() {
+    // the first regex has the highest priority
+    #[derive(Clone, Copy, Sana, PartialEq, Debug)]
+    enum AmbigToken {
+        #[regex(".+", priority = 1)]
+        A,
+        #[regex(".+")] // default priority = 0
+        #[allow(dead_code)]
+        B, // this one should not be constructed
+
+        #[error]
+        Error,
+    }
+
+    let input = "x";
+    let mut lexer = AmbigToken::lexer(&input);
+
+    let tok = lexer.next().unwrap();
+    assert_eq!(tok, Spanned{ value: AmbigToken::A, start: 0, end: 1 });
+}
+
+#[test]
+fn priority_last() {
+    // the last regex has the highest priority
+    #[derive(Clone, Copy, Sana, PartialEq, Debug)]
+    enum AmbigToken {
+        #[regex(".+")] // default priority = 0
+        #[allow(dead_code)]
+        A, // this one should not be constructed
+        #[regex(".+", priority = 1)]
+        B,
+
+        #[error]
+        Error,
+    }
+
+    let input = "x";
+    let mut lexer = AmbigToken::lexer(&input);
+
+    let tok = lexer.next().unwrap();
+    assert_eq!(tok, Spanned{ value: AmbigToken::B, start: 0, end: 1 });
+}
